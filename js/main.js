5729585461feb5731a69c7b0ee2c1ca066e2ce68
khojiakbar379 <khojiakbar739@gmail.com>
2021-07-04 21:20:51 +0500

    add all changes to html and css

---
+++ js/main.js
@@ -0,0 +1,27 @@
+alert('Wecome to our form!');
+
+var firstName = prompt ('write your name please!:' , 'Khojiakbar');
+var secondName = prompt ('write your second name :', 'Toshtemirov');
+var studyPlace = prompt ('write your study place :', 'Narxoz');
+var hometown = prompt ('write your Hometown :', 'Uzbekistan,Tashkent');
+var hobby = prompt ('write your hobby :', 'playing computer-games');
+var mobilePhoneBrand = prompt ('write your best mobile phone brand :', 'Samsung');
+var bestPlace = prompt ('write your best place :', 'home');
+var schoolNumber = prompt ('write your school number :', '250');
+var favouriteSubject = prompt ('write your favourite subject :', 'English');
+var age = prompt ('write your age :', '30');
+
+
+
+console.log (
+    'name: ' + firstName +
+'\nsecondName: '+secondName+
+"\nStudyPlace: " + studyPlace+
+"\nhometown: " + studyPlace+
+"\nhobby: " + studyPlace+
+"\nmobilePhoneBrand: " + studyPlace+
+"\nbestPlace: " + studyPlace+
+"\nschoolNumber: " + studyPlace+
+"\nfavouriteSubject: " + studyPlace+
+"\nage: " + studyPlace
+)
